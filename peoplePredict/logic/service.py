49978a1cec1701dc2be16f3eeae53a9accb8efbc
151250176 <151250176@smail.nju.edu.cn>
2019-12-11 19:09:30 +0800

    fix bug

--- peoplePredict/logic/service.py
+++ peoplePredict/logic/service.py
@@ -13,10 +13,11 @@ DAY = 1
 WEEK = 2
 
 INTEGRATION_DATABASE = 'integrated_result'
+DUMP_DATA_FILE = './peoplePredict/model/data/dao_service/position_name_map.pkl'
 
 # env
 dao = Dao()
-position_name = build_position_to_name()
+position_name = build_position_to_name(DUMP_DATA_FILE)
 
 
 # interface
@@ -28,45 +29,9 @@ def get_map_data(month, day, hour, aggregate):
             'Invalid date param. Month: ' + str(month) + ' day: ' + str(day) + ' hour: ' + str(hour))
 
     # if status == CURRENT:
-    res = []
-    if aggregate == HOUR:
-        for row in dao.read_data_from_target_database(INTEGRATION_DATABASE, {'month': month,
-                                                                             'day': day,
-                                                                             'hour': hour}):
-            res.append({'lng': row['lng_gcj02'], 'lat': row['lat_gcj02'], 'val': row['value']})
-    elif aggregate == DAY:
-        # 按天聚合查询
-        aggregate_map = {}
-        for row in dao.read_data_from_target_database(INTEGRATION_DATABASE, {'month': month,
-                                                                             'day': day}):
-            key = build_key(row)
-            if key in aggregate_map:
-                aggregate_map[key] += row['value']
-            aggregate_map[key] = row['value']
-        for key in aggregate_map:
-            lng, lat = to_data(key)
-            res.append({'lng': lng, 'lat': lat, 'val': aggregate_map[key]})
-    elif aggregate == WEEK:
-        # 按周聚合查询
-        aggregate_map = {}
-        cur_date = datetime.date(2019, month, day)
-        cur_date -= datetime.timedelta(days=3)
-        res = []
-        for i in range(7):
-            month = cur_date.month
-            day = cur_date.day
-            for row in dao.read_data_from_target_database(INTEGRATION_DATABASE, {'month': month,
-                                                                                 'day': day}):
-                key = build_key(row)
-                if key in aggregate_map:
-                    aggregate_map[key] += row['value']
-                aggregate_map[key] = row['value']
+    res = get_aggregate_position(aggregate, day, hour, month)
 
-            cur_date += datetime.timedelta(days=1)
-        for key in aggregate_map:
-            lng, lat = to_data(key)
-            res.append({'lng': lng, 'lat': lat, 'val': aggregate_map[key]})
-    else:
+    if len(res) == 0:
         return build_error_resp(
             'Invalid aggregate param. aggregate: ' + str(aggregate))
 
@@ -170,51 +135,24 @@ def get_top_ten_street(month, day, hour, aggregate):
         return build_error_resp(
             'Invalid date param. Month: ' + str(month) + ' day: ' + str(day) + ' hour: ' + str(hour))
 
-    position_map = {}
-    if aggregate == HOUR:
-        for row in dao.read_data_from_target_database(INTEGRATION_DATABASE, {'month': month,
-                                                                             'day': day,
-                                                                             'hour': hour}):
-            key = row['name']
-            if key in position_map:
-                position_map[key] = position_map[key] + row['value']
-            else:
-                position_map[key] = row['value']
-    elif aggregate == DAY:
-        # 按天聚合查询
-        for row in dao.read_data_from_target_database(INTEGRATION_DATABASE, {'month': month,
-                                                                             'day': day}):
-            key = row['name']
-            if key in position_map:
-                position_map[key] = position_map[key] + row['value']
-            else:
-                position_map[key] = row['value']
-    elif aggregate == WEEK:
-        # 按周聚合查询
-        cur_date = datetime.date(2019, month, day)
-        cur_date -= datetime.timedelta(days=3)
-        for i in range(7):
-            month = cur_date.month
-            day = cur_date.day
-            for row in dao.read_data_from_target_database(INTEGRATION_DATABASE, {'month': month,
-                                                                                 'day': day}):
-                key = row['name']
-                if key in position_map:
-                    position_map[key] = position_map[key] + row['value']
-                else:
-                    position_map[key] = row['value']
-            cur_date += datetime.timedelta(days=1)
-    else:
+    res = get_aggregate_position(aggregate, day, hour, month)
+
+    if len(res) == 0:
         return build_error_resp(
             'Invalid aggregate param. aggregate: ' + str(aggregate))
 
-    res = []
-    for key in position_map:
-        res.append((key, position_map[key]))
-    res.sort(key=lambda x: x[1], reverse=True)
+    res.sort(key=lambda x: x['val'], reverse=True)
+
+    out = []
+    for i in range(10):
+        cur = res[i]
+        key = build_key_by_lng_lat(cur['lng'], cur['lat'])
+        name = position_name.get(key, '未知地点')
+        out.append({'name': name,
+                    'val': cur['val']})
 
     return {'success': True,
-            'data': res[0:10]}
+            'data': out}
 
 
 # helper
@@ -271,9 +209,56 @@ def build_key(row):
     return str(row['lng_gcj02']) + ',' + str(row['lat_gcj02'])
 
 
+def build_key_by_lng_lat(lng, lat):
+    return str(lng) + ',' + str(lat)
+
+
 def to_data(key):
     lng, lat = key.split(',')
     return round(float(lng), 3), round(float(lat), 3)
+
+
+def get_aggregate_position(aggregate, day, hour, month):
+    res = []
+    if aggregate == HOUR:
+        for row in dao.read_data_from_target_database(INTEGRATION_DATABASE, {'month': month,
+                                                                             'day': day,
+                                                                             'hour': hour}):
+            res.append({'lng': row['lng_gcj02'], 'lat': row['lat_gcj02'], 'val': row['value']})
+    elif aggregate == DAY:
+        # 按天聚合查询
+        aggregate_map = {}
+        for row in dao.read_data_from_target_database(INTEGRATION_DATABASE, {'month': month,
+                                                                             'day': day}):
+            key = build_key(row)
+            if key in aggregate_map:
+                aggregate_map[key] += row['value']
+            aggregate_map[key] = row['value']
+        for key in aggregate_map:
+            lng, lat = to_data(key)
+            res.append({'lng': lng, 'lat': lat, 'val': aggregate_map[key]})
+    elif aggregate == WEEK:
+        # 按周聚合查询
+        aggregate_map = {}
+        cur_date = datetime.date(2019, month, day)
+        cur_date -= datetime.timedelta(days=3)
+        res = []
+        for i in range(7):
+            month = cur_date.month
+            day = cur_date.day
+            for row in dao.read_data_from_target_database(INTEGRATION_DATABASE, {'month': month,
+                                                                                 'day': day}):
+                key = build_key(row)
+                if key in aggregate_map:
+                    aggregate_map[key] += row['value']
+                else:
+                    aggregate_map[key] = row['value']
+
+            cur_date += datetime.timedelta(days=1)
+        for key in aggregate_map:
+            lng, lat = to_data(key)
+            res.append({'lng': lng, 'lat': lat, 'val': aggregate_map[key]})
+    return res
 
 
 if __name__ == '__main__':
--- peoplePredict/model/util/dao_service_util.py
+++ peoplePredict/model/util/dao_service_util.py
@@ -7,9 +7,13 @@ from peoplePredict.database.dao import Dao
 DUMP_FILE = '../data/dao_service/position_name_map.pkl'
 
 
-def build_position_to_name():
-    if os.path.exists(DUMP_FILE):
-        with open(DUMP_FILE, 'rb') as file:
+def build_position_to_name(file_name=None):
+    if file_name is None:
+        file_name = DUMP_FILE
+
+    print(os.path.abspath(file_name))
+    if os.path.exists(file_name):
+        with open(file_name, 'rb') as file:
             return pickle.load(file)
 
     print("we don't have dump data. build from script, please wait...")
